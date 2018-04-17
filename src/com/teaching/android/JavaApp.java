65a7de4e0d611bb58703e61b49941f699bbc3490
Juan Antonio Durán Ramírez <31082015+juanantoniomkt@users.noreply.github.com>
2018-04-17 13:03:19 +0200

    Mi primer Commit.

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,7 +6,28 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+
+            int valor1 = 12;
+            double valor2 = 12.12;
+            String valor3 = "Juan Antonio";
+            float valor4 = 12.2f;
+            boolean valor5 = false;
+            byte valor6 = 1;
+            long valor7 = 234342334554354l;
+            short valor8 = 234;
+            char valor9 = 'A';
+
+
+            System.out.println("string: " + valor3);
+            System.out.println("int: " + valor1);
+            System.out.println("double: " + valor2);
+            System.out.println("float: " + valor4);
+            System.out.println("boolean: " + valor5);
+            System.out.println("byte: " + valor6);
+            System.out.println("long: " + valor7);
+            System.out.println("short: " + valor8);
+            System.out.println("char: " + valor9);
+
     }
 
 }
