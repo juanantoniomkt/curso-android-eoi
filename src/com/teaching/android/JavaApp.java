5b4e5ad73b3b2752f746a7141b6fe14d81d9d31e
Juan Antonio Durán Ramírez <31082015+juanantoniomkt@users.noreply.github.com>
2018-04-18 11:12:05 +0200

    Práctica for, imprimir los primeros 20 números pares

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -11,52 +11,12 @@ public class JavaApp {
 
         int cont = 0;
 
-        int numeroAleatorio = (int) (Math.random()* 10 + 1);
+       for(int i = 0; i < 40; i = i+2)
+       {
+           cont = cont + 1;
 
-        boolean acertado = false;
-        String jugar = "no";
-
-        do {
-
-            cont = cont + 1;
-
-            Scanner keyborad = new Scanner(System.in);
-
-            System.out.println("Escribe un número: ");
-            System.out.println("------------------------------------------------");
-            int valor = keyborad.nextInt();
-
-            if(valor > numeroAleatorio)
-            {
-                System.out.println("es aleatorio es menor!!");
-            }
-
-            else if(valor < numeroAleatorio)
-            {
-                System.out.println("es aleatorio es mayor!!");
-            }
-
-            else
-            {
-                acertado = true;
-                System.out.println("Bien!! Has acertado");
-            }
-
-            if(acertado == true || cont == 3)
-            {
-                cont = 0;
-                System.out.println("repetir el juego??");
-
-                jugar = keyborad.next();
-
-                if (jugar.equals("no"))
-                {
-                    break;
-                }
-            }
-
-        } while(jugar != "si");
-
+           System.out.println(cont + " - num " + i);
+       }
 
     }
 
